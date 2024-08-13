aae2c2ccd335400fdfbeefa040bb346e2bb3b812
DangLe <lehaidangdev@gmail.com>
2024-08-13 16:23:08 +0700

    ci: update regex

--- check_title.sh
+++ check_title.sh
@@ -1,9 +1,14 @@
-title="fix: asdasdasd"
+title="test"
 
 #Define allowed Conventional Commit types
 allowed_types="feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert"
                              
-if ! echo "$title" | grep -Eq "^($allowed_types): #[0-9]+$"; then
-    echo "::error::Pull request title does not follow the required '<Conventional Commit type>: #<issue_number>' format"
+# if ! echo "$title" | grep -Eq "^($allowed_types): #[0-9]+$"; then
+#     echo "::error::Pull request title does not follow the required '<Conventional Commit type>: #<issue_number>' format"
+#     exit 1
+# fi
+
+if ! echo "$title" | grep -Eq "^($allowed_types)(?:\(.+\))?: (?:#[0-9]+|.*)$"; then
+    echo "::error::Pull request title does not follow the required format"
     exit 1
 fi
